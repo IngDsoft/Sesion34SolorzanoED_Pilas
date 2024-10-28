c848a1473c7ee8fdab3ca09a1739a653994699e7
IngDsoft <diego_solorzano117@hotmail.com>
2024-10-28 15:35:38 -0600

    confirmacion 1 ok solorzano
    datos de desarrollo: Solorzano

---
+++ Sesion34SolorzanoED_Pilas/src/sesion34solorzanoed_pilas/Sesion34SolorzanoED_Pilas.java
@@ -0,0 +1,25 @@
+
+package sesion34solorzanoed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+
+public class Sesion34SolorzanoED_Pilas {
+
+    
+    public static void main(String[] args) {
+        // Declaracion pila
+        Stack<Integer> pilaSolorzano = new Stack<>();
+        
+        pilaSolorzano.push(100);
+        pilaSolorzano.push(150);
+        pilaSolorzano.push(230);
+        pilaSolorzano.push(10);
+        pilaSolorzano.push(160);
+        
+        JOptionPane.showMessageDialog(null, "Datos en pila ordenada es :\n" + pilaSolorzano);
+        
+    }
+    
+}
